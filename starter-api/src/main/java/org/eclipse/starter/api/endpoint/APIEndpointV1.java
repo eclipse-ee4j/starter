432d884c842d1c994f87dd6da6ad26f61706aff0
Jeyvison Nascimento <jeynoronha@gmail.com>
2020-04-14 12:08:34 -0300

    #4 Adjust Copyright headers

--- starter-api/src/main/java/org/eclipse/starter/api/endpoint/APIEndpointV1.java
+++ starter-api/src/main/java/org/eclipse/starter/api/endpoint/APIEndpointV1.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright (c) 2020 Jeyvison Nascimento and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Jeyvison Nascimento - initial API and implementation
+ ********************************************************************************/
+
 package org.eclipse.starter.api.endpoint;
 
 
