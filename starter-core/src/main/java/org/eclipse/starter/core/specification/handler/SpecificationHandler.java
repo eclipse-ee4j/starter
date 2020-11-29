bb4516cbe5dadee638abd2fcc4025bb53820ffbb
Jeyvison Nascimento <jeynoronha@gmail.com>
2020-11-29 16:58:03 -0300

    Fix missing Copyright and add default method

--- starter-core/src/main/java/org/eclipse/starter/core/specification/handler/SpecificationHandler.java
+++ starter-core/src/main/java/org/eclipse/starter/core/specification/handler/SpecificationHandler.java
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright (c) 2020 Jeyvison Nascimento and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Jeyvison Nascimento - Create Specification Handler
+ ********************************************************************************/
+
 package org.eclipse.starter.core.specification.handler;
 
 import org.eclipse.starter.core.ThymeleafEngine;
@@ -9,4 +22,8 @@ import java.util.Map;
 public interface SpecificationHandler {
 
     public void handle(Project project, ThymeleafEngine thymeleafEngine, Map<String, Object> variables, ZipCreator zipCreator);
+
+    public default String getPackageAsDirectory(String packageName){
+        return "/" + packageName.replace(".", "/");
+    }
 }
