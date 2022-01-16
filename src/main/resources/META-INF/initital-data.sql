a73fd8f9331badd9e71350401fa808b8a55dfdcc
Reza Rahman <reza_rahman@lycos.com>
2022-01-16 15:11:35 -0500

    Add initial data.

---
+++ src/main/resources/META-INF/initital-data.sql
@@ -0,0 +1,3 @@
+INSERT INTO coffee (id, name, price) VALUES (1, 'Coffee-1', 2.75);
+INSERT INTO coffee (id, name, price) VALUES (2, 'Coffee-2', 1.99);
+INSERT INTO coffee (id, name, price) VALUES (3, 'Coffee-3', 3.25);
