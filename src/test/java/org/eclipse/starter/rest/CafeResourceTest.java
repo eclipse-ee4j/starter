0391fb57fe6e346a7f40eb4bb06135028809a199
Reza Rahman <reza_rahman@lycos.com>
2022-02-13 18:05:30 -0500

    Test add coffee.

--- src/test/java/org/eclipse/starter/rest/CafeResourceTest.java
+++ src/test/java/org/eclipse/starter/rest/CafeResourceTest.java
@@ -1,42 +1,72 @@
 package org.eclipse.starter.rest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 
 import org.eclipse.starter.model.CafeRepository;
 import org.eclipse.starter.model.entity.Coffee;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.transaction.api.annotation.Transactional;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-/**
- * Application layer integration test covering a number of otherwise fairly trivial components that
- * largely do not warrant their own tests.
- *
- * <p>Ensure a Payara instance is running locally before this test is executed, with the default
- * user name and password.
- */
+import fish.payara.arquillian.ws.rs.core.MediaType;
+
 @RunWith(Arquillian.class)
 public class CafeResourceTest {
 
-  @PersistenceContext private EntityManager entityManager;
+	private static final String BASE_URI = "http://localhost:9090/jakarta-starter-test/rest/coffees";
+	@PersistenceContext
+	private EntityManager entityManager;
 
-  @Deployment
-  public static WebArchive createDeployment() {
-    return ShrinkWrap.create(WebArchive.class, "jakarta-starter-test.war")
-        .addClass(CafeResource.class)
-        .addClass(CafeRepository.class)
-        .addClass(Coffee.class)
-        .addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
-        .addAsResource("META-INF/initital-data.sql", "META-INF/initital-data.sql")        
-        // Web application descriptor
-        .addAsWebInfResource("test-web.xml", "web.xml");
-  }
+	@Deployment
+	public static WebArchive createDeployment() {
+		return ShrinkWrap.create(WebArchive.class, "jakarta-starter-test.war").addClass(CafeResource.class)
+				.addClass(CafeRepository.class).addClass(Coffee.class)
+				.addAsResource("test-persistence.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource("test-web.xml", "web.xml");
+	}
 
-  @Test
-  public void testGetAllCoffees() {
-  }
+	@Test
+	@Transactional
+	public void testAddCoffee() {
+		Coffee coffee = null;
+
+		TypedQuery<Coffee> query = entityManager.createQuery("SELECT o FROM Coffee o WHERE o.name = :name",
+				Coffee.class);
+		query.setParameter("name", "Test-1");
+
+		try {
+			coffee = query.getSingleResult();
+			fail("No entity should have been found.");
+		} catch (NoResultException ne) {
+			// Expected
+		}
+
+		coffee = new Coffee("Test-1", 7.25);
+		ClientBuilder.newClient().target(BASE_URI).request(MediaType.APPLICATION_JSON).post(Entity.json(coffee));
+
+		query = entityManager.createQuery("SELECT o FROM Coffee o WHERE o.name = :name", Coffee.class);
+		query.setParameter("name", "Test-1");
+
+		coffee = query.getSingleResult();
+
+		assertNotNull(coffee);
+		assertEquals(coffee.getName(), "Test-1");
+		assertEquals(coffee.getPrice().doubleValue(), 7.25, 0);
+		
+		entityManager.createQuery("DELETE FROM Coffee").executeUpdate();
+	}
+	
 }
