41db59ef7c7316102d5395a1530c8e28b00b9b1d
Reza Rahman <reza_rahman@lycos.com>
2022-02-13 20:36:34 -0500

    Update initital-data.sql

--- src/main/resources/META-INF/initital-data.sql
+++ src/main/resources/META-INF/initital-data.sql
@@ -1,3 +1,3 @@
-INSERT INTO coffee (id, name, price) VALUES (1, 'Coffee-1', 2.75);
-INSERT INTO coffee (id, name, price) VALUES (2, 'Coffee-2', 1.99);
-INSERT INTO coffee (id, name, price) VALUES (3, 'Coffee-3', 3.25);
+INSERT INTO coffee (id, name, price) VALUES (1, 'Coffee-A', 2.75);
+INSERT INTO coffee (id, name, price) VALUES (2, 'Coffee-B', 1.99);
+INSERT INTO coffee (id, name, price) VALUES (3, 'Coffee-C', 3.25);
