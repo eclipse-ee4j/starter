7a902037649f3aca5a352b3c3e446c3d48979a32
Reza Rahman <reza_rahman@lycos.com>
2022-03-21 18:46:35 -0400

    Add support for GlassFish

---
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,17 @@
+import org.apache.commons.io.FileUtils
+
+def outputDirectory = new File(request.getOutputDirectory(), request.getArtifactId())
+def glassFishDirectory = new File(outputDirectory, "glassfish")
+
+def runtime = request.properties["runtime"].trim().toLowerCase()
+
+switch (runtime)
+{
+    case "glassfish": println "Generating code for GlassFish"
+                      FileUtils.copyDirectory(glassFishDirectory, outputDirectory)
+                      FileUtils.forceDelete(new File(outputDirectory, "Dockerfile"))
+                      break
+    default: println "Generating code for Payara"
+}
+
+FileUtils.forceDelete(glassFishDirectory)
