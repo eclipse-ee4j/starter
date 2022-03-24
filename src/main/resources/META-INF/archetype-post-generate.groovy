4dc4a356afdbd250dfa708373bfd75cd2d25e766
A N M Bazlur Rahman <bazlur@jugbd.org>
2022-03-24 19:22:05 -0400

    Add support from WildFly

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -2,6 +2,7 @@ import org.apache.commons.io.FileUtils
 
 def outputDirectory = new File(request.getOutputDirectory(), request.getArtifactId())
 def glassFishDirectory = new File(outputDirectory, "glassfish")
+def wildflyDirectory = new File(outputDirectory, "wildfly")
 
 def runtime = request.properties["runtime"].trim().toLowerCase()
 
@@ -11,7 +12,13 @@ switch (runtime)
                       FileUtils.copyDirectory(glassFishDirectory, outputDirectory)
                       FileUtils.forceDelete(new File(outputDirectory, "Dockerfile"))
                       break
+
+    case "wildfly": println "Generating code for wildfly"
+                    FileUtils.copyDirectory(wildflyDirectory, outputDirectory)
+                    FileUtils.forceDelete(new File(outputDirectory, "Dockerfile"))
+                    break;
     default: println "Generating code for Payara"
 }
 
 FileUtils.forceDelete(glassFishDirectory)
+FileUtils.forceDelete(wildflyDirectory)
