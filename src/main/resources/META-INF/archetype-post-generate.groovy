2adbc1e17e44a47d25eb0e9524dca544dc488355
Reza Rahman <reza_rahman@lycos.com>
2022-04-03 16:51:57 -0400

    Add TomEE support.

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -2,6 +2,7 @@ import org.apache.commons.io.FileUtils
 
 def outputDirectory = new File(request.getOutputDirectory(), request.getArtifactId())
 def glassFishDirectory = new File(outputDirectory, "glassfish")
+def tomEEDirectory = new File(outputDirectory, "tomee")
 
 def runtime = request.properties["runtime"].trim().toLowerCase()
 
@@ -11,7 +12,13 @@ switch (runtime)
                       FileUtils.copyDirectory(glassFishDirectory, outputDirectory)
                       FileUtils.forceDelete(new File(outputDirectory, "Dockerfile"))
                       break
-    default: println "Generating code for Payara"
+                      
+    case "tomee":     println "Generating code for TomEE"
+                      FileUtils.copyDirectory(tomEEDirectory, outputDirectory)
+                      break                      
+                      
+    default:          println "Generating code for Payara"
 }
 
 FileUtils.forceDelete(glassFishDirectory)
+FileUtils.forceDelete(tomEEDirectory)
