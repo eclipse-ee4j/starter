8bf5a83c17094d4472692deaf289a4d8befdea58
Reza Rahman <reza_rahman@lycos.com>
2022-04-04 18:20:18 -0400

    Remove TomEE

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -2,7 +2,6 @@ import org.apache.commons.io.FileUtils
 
 def outputDirectory = new File(request.getOutputDirectory(), request.getArtifactId())
 def glassFishDirectory = new File(outputDirectory, "glassfish")
-def tomEEDirectory = new File(outputDirectory, "tomee")
 
 def runtime = request.properties["runtime"].trim().toLowerCase()
 
@@ -13,12 +12,7 @@ switch (runtime)
                       FileUtils.forceDelete(new File(outputDirectory, "Dockerfile"))
                       break
                       
-    case "tomee":     println "Generating code for TomEE"
-                      FileUtils.copyDirectory(tomEEDirectory, outputDirectory)
-                      break                      
-                      
     default:          println "Generating code for Payara"
 }
 
 FileUtils.forceDelete(glassFishDirectory)
-FileUtils.forceDelete(tomEEDirectory)
