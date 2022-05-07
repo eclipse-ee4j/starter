711a8dba1e2c630e6ca07c1a73c8c2b3c0437338
Jeyvison Nascimento <jeynoronha@gmail.com>
2022-05-07 22:42:37 +0100

    Remove Dockerfile from Payara project when jakarta version is 9

--- src/main/resources/META-INF/archetype-post-generate.groovy
+++ src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,10 +1,13 @@
 import org.apache.commons.io.FileUtils
 
+import java.nio.file.Files
+
 def outputDirectory = new File(request.getOutputDirectory(), request.getArtifactId())
 def glassFishDirectory = new File(outputDirectory, "glassfish")
 def tomEEDirectory = new File(outputDirectory, "tomee")
 
 def runtime = request.properties["runtime"].trim().toLowerCase()
+def jakartaVersion = request.properties["jakartaVersion"].trim()
 
 switch (runtime)
 {
@@ -18,6 +21,8 @@ switch (runtime)
                       break                      
                       
     default:          println "Generating code for Payara"
+                      if ("9" == jakartaVersion) FileUtils.forceDelete(new File(outputDirectory, "Dockerfile"))
+
 }
 
 FileUtils.forceDelete(glassFishDirectory)
