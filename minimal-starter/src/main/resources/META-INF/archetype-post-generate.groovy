cc91cc18d9b5621158b2ef5c8435ef5377fe338c
Reza Rahman <reza_rahman@lycos.com>
2022-05-07 22:09:48 -0400

    Adding Maven wrapper.

--- minimal-starter/src/main/resources/META-INF/archetype-post-generate.groovy
+++ minimal-starter/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,5 +1,6 @@
 import org.apache.commons.io.FileUtils
 
+// Processing target runtime specific code.
 def outputDirectory = new File(request.getOutputDirectory(), request.getArtifactId())
 def glassFishDirectory = new File(outputDirectory, "glassfish")
 def tomEEDirectory = new File(outputDirectory, "tomee")
@@ -22,3 +23,22 @@ switch (runtime)
 
 FileUtils.forceDelete(glassFishDirectory)
 FileUtils.forceDelete(tomEEDirectory)
+
+// Adding Maven Wrapper
+println "Adding Maven Wrapper"
+
+def pomFile = new File(outputDirectory, "pom.xml")
+def isWindows = System.properties['os.name'].toLowerCase().contains('windows')
+def starter = isWindows ? "cmd.exe" : "/bin/sh"
+def switcher = isWindows ? "/c" : "-c"
+def command = "mvn -f \"${pomFile.getAbsolutePath()}\" wrapper:wrapper"
+
+def output = new StringBuilder()
+def proc = [starter, switcher, command].execute();
+proc.consumeProcessOutput(output, output)
+proc.waitFor()
+
+if (proc.exitValue() != 0 || output == null || !output.contains("BUILD SUCCESS")) {
+    println("${output}")
+    throw new RuntimeException("Failed to generate code from archetype.")
+}
