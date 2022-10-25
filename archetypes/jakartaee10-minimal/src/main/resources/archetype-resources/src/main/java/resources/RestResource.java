9f9763c302563f1591884c372ba0ed962bda6090
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2022-10-25 12:49:03 +0200

    Update RestResource.java

--- archetypes/jakartaee10-minimal/src/main/resources/archetype-resources/src/main/java/resources/RestResource.java
+++ archetypes/jakartaee10-minimal/src/main/resources/archetype-resources/src/main/java/resources/RestResource.java
@@ -18,7 +18,6 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.Response;
 
 @Path("hello")
 public class RestResource {
