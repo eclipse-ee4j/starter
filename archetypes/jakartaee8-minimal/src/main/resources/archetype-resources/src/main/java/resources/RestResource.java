be8e5abfd74a75c93db99a386092c0b3d072c497
Ivar Grimstad <ivar.grimstad@eclipse-foundation.org>
2022-10-25 09:24:23 +0200

    Add Jakarta EE 8 Minimal Archetype

---
+++ archetypes/jakartaee8-minimal/src/main/resources/archetype-resources/src/main/java/resources/RestResource.java
@@ -0,0 +1,31 @@
+/********************************************************************************
+ * Copyright (c) 10/19/2022 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+ ********************************************************************************/
+package ${package}.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("hello")
+public class RestResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hello hello(){
+        return new Hello("Hello from Jakarta EE");
+    }
+}
