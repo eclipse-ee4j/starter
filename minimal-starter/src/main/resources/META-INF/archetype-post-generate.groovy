17f919b2fb4d07203b17c51718a3ed61806a7fc8
Reza Rahman <reza_rahman@lycos.com>
2023-01-16 17:29:41 -0500

    Deprecate minimal Archetype

--- minimal-starter/src/main/resources/META-INF/archetype-post-generate.groovy
+++ minimal-starter/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -39,3 +39,10 @@ if (proc.exitValue() != 0 || output == null || !output.contains("BUILD SUCCESS")
     println("${output}")
     throw new RuntimeException("Failed to generate code from archetype.")
 }
+
+deprecated(outputDirectory)
+
+private deprecated(File outputDirectory) {
+    FileUtils.forceDelete(outputDirectory)
+    throw new RuntimeException("Failed, this Archetype is deprecated in 2.0. Please use the jakarta-starter Archetype instead.")
+}
