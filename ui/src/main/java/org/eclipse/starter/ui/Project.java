6e62eaccf6b044af64b3372b3440825e26eabe35
Reza Rahman <reza_rahman@lycos.com>
2023-02-25 15:37:06 -0500

    Cleanup

--- ui/src/main/java/org/eclipse/starter/ui/Project.java
+++ ui/src/main/java/org/eclipse/starter/ui/Project.java
@@ -1,143 +1,87 @@
 package org.eclipse.starter.ui;
 
+import static java.util.Map.entry;
 
-import static java.util.logging.Level.SEVERE;
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.lang.invoke.MethodHandles;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-import java.util.logging.Logger;
 
 @Named
 @RequestScoped
 public class Project {
 
-  private static final Logger logger = Logger.getLogger(
-      MethodHandles.lookup().lookupClass().getName());
-  private static final String ARCHETYPE_GROUP_ID = "org.eclipse.starter";
-  private static final String ARCHETYPE_VERSION = "1.1.0";
+	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
+	private static final Map<String, String> RUNTIMES = Map.ofEntries(entry("GlassFish", "glassfish"),
+			entry("Open Liberty", "open-liberty"), entry("Payara", "payara"), entry("TomEE", "tomee"),
+			entry("WildFly", "wildfly"));
 
-  private JakartaEEVersion jakartaEEVersion;
-  private Profile profile;
-  private JavaSEVersion javaSEVersion;
-  private ApplicationType applicationType;
-  private Set<ApplicationFeature> applicationFeatures = new HashSet<ApplicationFeature>();
-  private JakartaRuntime jakartaRuntime;
-  private String groupId = "com.example";
-  private String artifactId = "demo";
-  private String version = "1.0.0-SNAPSHOT"; //default value
+	private double jakartaVersion;
+	private String profile;
+	private int javaVersion;
+	private boolean docker;
+	private String runtime;
 
-  public JakartaEEVersion getJakartaEEVersion() {
-    return jakartaEEVersion;
-  }
+	public double getJakartaVersion() {
+		return jakartaVersion;
+	}
 
-  public void setJakartaEEVersion(final JakartaEEVersion jakartaEEVersion) {
-    this.jakartaEEVersion = jakartaEEVersion;
-  }
+	public void setJakartaVersion(double jakartaVersion) {
+		this.jakartaVersion = jakartaVersion;
+	}
 
-  public String getGroupId() {
-    return groupId;
-  }
+	public String getProfile() {
+		return profile;
+	}
 
-  public void setGroupId(final String groupId) {
-    this.groupId = groupId;
-  }
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
 
-  public String getArtifactId() {
-    return artifactId;
-  }
+	public int getJavaVersion() {
+		return javaVersion;
+	}
 
-  public void setArtifactId(final String artifactId) {
-    this.artifactId = artifactId;
-  }
+	public void setJavaVersion(int javaVersion) {
+		this.javaVersion = javaVersion;
+	}
 
-  public String getVersion() {
-    return version;
-  }
+	public boolean isDocker() {
+		return docker;
+	}
 
-  public void setVersion(final String version) {
-    this.version = version;
-  }
+	public void setDocker(boolean docker) {
+		this.docker = docker;
+	}
 
-  public Profile getProfile() {
-    return profile;
-  }
+	public String getRuntime() {
+		return runtime;
+	}
 
-  public void setProfile(final Profile profile) {
-    this.profile = profile;
-  }
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
 
-  public JavaSEVersion getJavaSEVersion() {
-    return javaSEVersion;
-  }
+	public Map<String, String> getRuntimes() {
+		List<String> runtimes = new ArrayList<>(RUNTIMES.keySet());
+		Collections.shuffle(runtimes);
 
-  public void setJavaSEVersion(final JavaSEVersion javaSEVersion) {
-    this.javaSEVersion = javaSEVersion;
-  }
+		Map<String, String> shuffledRuntimes = new LinkedHashMap<>();
+		runtimes.forEach(r -> shuffledRuntimes.put(r, RUNTIMES.get(r)));
 
-  public Set<ApplicationFeature> getApplicationFeatures() {
-    return applicationFeatures;
-  }
+		return shuffledRuntimes;
+	}
 
-  public void setApplicationFeatures(
-      final Set<ApplicationFeature> applicationFeatures) {
-    this.applicationFeatures = applicationFeatures;
-  }
-
-  public JakartaRuntime getJakartaRuntime() {
-    return jakartaRuntime;
-  }
-
-  public void setJakartaRuntime(final JakartaRuntime jakartaRuntime) {
-    this.jakartaRuntime = jakartaRuntime;
-  }
-
-  public ApplicationType getApplicationType() {
-    return applicationType;
-  }
-
-  public void setApplicationType(final ApplicationType applicationType) {
-    this.applicationType = applicationType;
-  }
-
-  public void generate() {
-    final String downloadUrl = String.format(
-        "/download.zip?archetypeGroupId=%s&archetypeArtifactId=%s&archetypeVersion=%s&groupId=%s&artifactId=%s&profile=%s&version=%s",
-        ARCHETYPE_GROUP_ID, jakartaEEVersion.getArcheTypeArtifactId(), ARCHETYPE_VERSION, groupId,
-        artifactId, profile.getProfile(), version);
-
-    FacesContext context = FacesContext.getCurrentInstance();
-    HttpServletResponse response = (HttpServletResponse) context.getExternalContext().getResponse();
-    context.responseComplete();
-    try {
-      final Object redirectUrl = getApplicationUri()
-          .map(root -> root + downloadUrl)
-          .orElse(downloadUrl);
-      response.sendRedirect(redirectUrl + "?faces-redirect=true");
-    } catch (IOException e) {
-      logger.log(SEVERE, "Was not able to generate file", e);
-      throw new RuntimeException(e);
-    }
-  }
-
-  private Optional<String> getApplicationUri() {
-    try {
-      var ext = FacesContext.getCurrentInstance().getExternalContext();
-      var uri = new URI(ext.getRequestScheme(),
-          null, ext.getRequestServerName(), ext.getRequestServerPort(),
-          ext.getRequestContextPath(), null, null);
-      return Optional.of(uri.toASCIIString());
-    } catch (URISyntaxException e) {
-      logger.log(SEVERE, "Was not able to get context path", e);
-      return Optional.empty();
-    }
-  }
+	public void generate() {
+		LOGGER.info("Generating project");
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.responseComplete();
+	}
 }
