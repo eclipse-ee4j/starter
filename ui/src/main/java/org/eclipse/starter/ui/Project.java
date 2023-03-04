64f35319764c11eb1c101d7cc7df5771858964f6
Reza Rahman <reza_rahman@lycos.com>
2023-03-04 18:49:22 -0500

    Add initial validation

--- ui/src/main/java/org/eclipse/starter/ui/Project.java
+++ ui/src/main/java/org/eclipse/starter/ui/Project.java
@@ -4,10 +4,13 @@ import static java.util.Map.entry;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,31 +18,69 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.enterprise.context.RequestScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
-@RequestScoped
-public class Project {
+@ViewScoped
+public class Project implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
-	private static final Map<String, String> RUNTIMES = Map.ofEntries(entry("GlassFish", "glassfish"),
-			entry("Open Liberty", "open-liberty"), entry("Payara", "payara"), entry("TomEE", "tomee"),
-			entry("WildFly", "wildfly"));
+	private static final Map<String, String> RUNTIMES = Map.ofEntries(entry("glassfish", "GlassFish"),
+			entry("open-liberty", "Open Liberty"), entry("payara", "Payara"), entry("tomee", "TomEE"),
+			entry("wildfly", "WildFly"));
 
 	@Inject
 	private FacesContext facesContext;
 	@Inject
 	private ExternalContext externalContext;
 
+	private Map<String, SelectItem> jakartaVersions = new LinkedHashMap<String, SelectItem>();
 	private double jakartaVersion = 10;
+
+	private Map<String, SelectItem> profiles = new LinkedHashMap<String, SelectItem>();
 	private String profile = "core";
+
+	private Map<String, SelectItem> javaVersions = new LinkedHashMap<String, SelectItem>();
 	private int javaVersion = 17;
 	private boolean docker = false;
+
+	private Map<String, SelectItem> runtimes = new LinkedHashMap<String, SelectItem>();
 	private String runtime = "none";
+
+	public Project() {
+		jakartaVersions.put("8", new SelectItem("8", "Jakarta EE 8"));
+		jakartaVersions.put("9", new SelectItem("9", "Jakarta EE 9"));
+		jakartaVersions.put("9.1", new SelectItem("9.1", "Jakarta EE 9.1"));
+		jakartaVersions.put("10", new SelectItem("10", "Jakarta EE 10"));
+
+		profiles.put("core", new SelectItem("core", "Core Profile"));
+		profiles.put("web", new SelectItem("web", "Web Profile"));
+		profiles.put("full", new SelectItem("full", "Full platform"));
+
+		javaVersions.put("8", new SelectItem("8", "Java SE 8", "Java SE 8", true));
+		javaVersions.put("11", new SelectItem("11", "Java SE 11"));
+		javaVersions.put("17", new SelectItem("17", "Java SE 17"));
+
+		runtimes.put("none", new SelectItem("none", "None"));
+
+		List<String> shuffledRuntimes = new ArrayList<>(RUNTIMES.keySet());
+		Collections.shuffle(shuffledRuntimes);
+		shuffledRuntimes.forEach(r -> runtimes.put(r, new SelectItem(r, RUNTIMES.get(r))));
+
+		runtimes.get("tomee").setDisabled(true); // EE 10 is the default. TomEE needs to be disabled.
+		runtimes.get("open-liberty").setDisabled(true); // EE 10 is the default. Open Liberty needs to be disabled.
+		runtimes.get("glassfish").setDisabled(true); // Core is the default. GlassFish needs to be disabled.
+	}
+
+	public Collection<SelectItem> getJakartaVersions() {
+		return jakartaVersions.values();
+	}
 
 	public double getJakartaVersion() {
 		return jakartaVersion;
@@ -49,12 +90,20 @@ public class Project {
 		this.jakartaVersion = jakartaVersion;
 	}
 
+	public Collection<SelectItem> getProfiles() {
+		return profiles.values();
+	}
+
 	public String getProfile() {
 		return profile;
 	}
 
 	public void setProfile(String profile) {
 		this.profile = profile;
+	}
+
+	public Collection<SelectItem> getJavaVersions() {
+		return javaVersions.values();
 	}
 
 	public int getJavaVersion() {
@@ -77,18 +126,162 @@ public class Project {
 		return runtime;
 	}
 
+	public Collection<SelectItem> getRuntimes() {
+		return runtimes.values();
+	}
+
 	public void setRuntime(String runtime) {
 		this.runtime = runtime;
 	}
 
-	public Map<String, String> getRuntimes() {
-		List<String> runtimes = new ArrayList<>(RUNTIMES.keySet());
-		Collections.shuffle(runtimes);
+	public void onJakartaVersionChange() {
+		LOGGER.log(Level.INFO,
+				"Validating form for Jakarta EE version: {0}, Jakarta EE profile: {1}, Java SE version: {2}, Docker: {3}, runtime: {4}",
+				new Object[] { jakartaVersion, profile, javaVersion, docker, runtime });
+		if (jakartaVersion != 10) {
+			javaVersions.get("8").setDisabled(false);
+			runtimes.get("tomee").setDisabled(false);
+			runtimes.get("open-liberty").setDisabled(false);
 
-		Map<String, String> shuffledRuntimes = new LinkedHashMap<>();
-		runtimes.forEach(r -> shuffledRuntimes.put(r, RUNTIMES.get(r)));
+			profiles.get("core").setDisabled(true);
+			if (profile.equals("core")) {
+				profile = "web";
+			}
 
-		return shuffledRuntimes;
+			if ((jakartaVersion == 9) || (jakartaVersion == 9.1)) {
+				runtimes.get("wildfly").setDisabled(true);
+				if (runtime.equals("wildfly")) {
+					runtime = "none";
+				}
+			} else {
+				runtimes.get("wildfly").setDisabled(false);
+			}
+		} else {
+			javaVersions.get("8").setDisabled(true);
+			if (javaVersion == 8) {
+				javaVersion = 11;
+			}
+
+			profiles.get("core").setDisabled(false);
+
+			runtimes.get("tomee").setDisabled(true);
+			if (runtime.equals("tomee")) {
+				runtime = "none";
+			}
+
+			runtimes.get("open-liberty").setDisabled(true);
+			if (runtime.equals("open-liberty")) {
+				runtime = "none";
+			}
+
+			runtimes.get("wildfly").setDisabled(false);
+		}
+	}
+
+	public void onProfileChange() {
+		LOGGER.log(Level.INFO,
+				"Validating form for Jakarta EE version: {0}, Jakarta EE profile: {1}, Java SE version: {2}, Docker: {3}, runtime: {4}",
+				new Object[] { jakartaVersion, profile, javaVersion, docker, runtime });
+		if (profile.equals("core")) {
+			runtimes.get("glassfish").setDisabled(true);
+			runtimes.get("tomee").setDisabled(true);
+
+			if ((runtime.equals("glassfish")) || (runtime.equals("tomee"))) {
+				runtime = "none";
+			}
+		} else if (profile.equals("full")) {
+			runtimes.get("tomee").setDisabled(true);
+			runtimes.get("glassfish").setDisabled(false);
+
+			if (runtime.equals("tomee")) {
+				runtime = "none";
+			}
+		} else {
+			runtimes.get("tomee").setDisabled(false);
+			runtimes.get("glassfish").setDisabled(false);
+		}
+	}
+
+	public void onJavaVersionChange() {
+		LOGGER.log(Level.INFO,
+				"Validating form for Jakarta EE version: {0}, Jakarta EE profile: {1}, Java SE version: {2}, Docker: {3}, runtime: {4}",
+				new Object[] { jakartaVersion, profile, javaVersion, docker, runtime });
+		if (javaVersion == 8) {
+			jakartaVersions.get("10").setDisabled(true);
+			if (jakartaVersion == 10) {
+				jakartaVersion = 9.1;
+			}
+
+			profiles.get("core").setDisabled(true);
+			if (profile.equals("core")) {
+				profile = "web";
+			}
+		} else {
+			jakartaVersions.get("10").setDisabled(false);
+		}
+	}
+
+	public void onDockerChange() {
+		LOGGER.log(Level.INFO,
+				"Validating form for Jakarta EE version: {0}, Jakarta EE profile: {1}, Java SE version: {2}, Docker: {3}, runtime: {4}",
+				new Object[] { jakartaVersion, profile, javaVersion, docker, runtime });
+		if (docker) {
+			runtimes.get("none").setDisabled(true);
+			runtimes.get("glassfish").setDisabled(true);
+
+			Iterator<String> iterator = runtimes.keySet().iterator();
+			while (runtime.equals("none") || runtime.equals("glassfish")) {
+				runtime = iterator.next();
+			}
+		} else {
+			runtimes.get("none").setDisabled(false);
+			runtimes.get("glassfish").setDisabled(false);
+		}
+	}
+
+	public void onRuntimeChange() {
+		LOGGER.log(Level.INFO,
+				"Validating form for Jakarta EE version: {0}, Jakarta EE profile: {1}, Java SE version: {2}, Docker: {3}, runtime: {4}",
+				new Object[] { jakartaVersion, profile, javaVersion, docker, runtime });
+		if (runtime.equals("none")) {
+			docker = false;
+		} else if (runtime.equals("payara")) {
+			if ((jakartaVersion != 8) && (javaVersion == 8)) {
+				javaVersion = 11;
+			}
+		} else if (runtime.equals("glassfish")) {
+			docker = false;
+
+			if (profile.equals("core")) {
+				profile = "web";
+			}
+
+			if ((jakartaVersion != 8) && (javaVersion == 8)) {
+				javaVersion = 11;
+			}
+		} else if (runtime.equals("tomee")) {
+			if (jakartaVersion == 10) {
+				jakartaVersion = 9.1;
+			}
+
+			profile = "web";
+
+			if ((jakartaVersion != 8) && (javaVersion == 8)) {
+				javaVersion = 11;
+			}
+		} else if (runtime.equals("wildfly")) {
+			if ((jakartaVersion == 9) || (jakartaVersion == 9.1)) {
+				jakartaVersion = 10;
+			}
+
+			if ((jakartaVersion == 10) && (javaVersion == 8)) {
+				javaVersion = 11;
+			}
+		} else if (runtime.equals("open-liberty")) {
+			if (jakartaVersion == 10) {
+				jakartaVersion = 9.1;
+			}
+		}
 	}
 
 	public void generate() {
@@ -100,7 +293,6 @@ public class Project {
 			File workingDirectory = Files.createTempDirectory("starter-output-").toFile();
 			LOGGER.log(Level.INFO, "Working directory: {0}", new Object[] { workingDirectory.getAbsolutePath() });
 
-			
 			LOGGER.info("Executing Maven Archetype.");
 			Properties properties = new Properties();
 			properties.putAll(Map.ofEntries(
