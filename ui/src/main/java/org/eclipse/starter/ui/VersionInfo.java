6b6cde2413907c8af8aef3ac777238f0ed46fced
Jeyvison Nascimento <jeynoronha@gmail.com>
2024-03-25 22:58:44 +0000

    Release UI

--- ui/src/main/java/org/eclipse/starter/ui/VersionInfo.java
+++ ui/src/main/java/org/eclipse/starter/ui/VersionInfo.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.Response;
 public class VersionInfo {
 
 	public static final String VERSION_PROPERTY = "version";
-	public static final String COMPILE_DEFAULT_ARCHETYPE_VERSION = "2.3.0";
+	public static final String COMPILE_DEFAULT_ARCHETYPE_VERSION = "2.2.3";
 	public static final String ARCHETYPE_VERSION_ENV_VAR = System.getenv("ARCHETYPE_VERSION"); 
 	public static final String ARCHETYPE_VERSION = ( ARCHETYPE_VERSION_ENV_VAR != null)
 			? System.getenv("ARCHETYPE_VERSION")
